fa8666e1412e4f27c06fc783540e04da30266475
Qiang Liu <cyruscyliu@gmail.com>
2019-11-03 16:05:14 +0800

    fix bugs

---
+++ tests/test_llvm_compile.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+from build.components.llvm_compile import LLVMCompile
+
+
+class TestLLVMCompile(TestCase):
+    def test_mips_31820(self):
+        kwargs = {
+            'makeout': '/home/liuqiang/Desktop/linux-3.18.20/makeout.txt',
+            'clangbin': '/usr/bin/clang-9',
+            'llvm_bc_out': '/home/liuqiang/Desktop/linux-3.18.20-llvm-bitcode',
+            'compiler_name': '/home/liuqiang/Desktop/staging_dir/toolchain-mips_34kc_gcc-4.8-linaro_uClibc-0.9.33.2/bin/mips-openwrt-linux-gcc',
+            'arch': 'mips',
+            'kernel_src_dir': '/home/liuqiang/Desktop/linux-3.18.20'
+        }
+        llvm_build = LLVMCompile(**kwargs)
+        status = llvm_build.setup()
+        if status is not None:
+            print(status)
+            return
+        llvm_build.perform()
