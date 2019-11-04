09034996b27094ac278f805269da48787e8c8e10
Qiang Liu <cyruscyliu@gmail.com>
2019-11-04 20:13:45 +0800

    fix bugs in linking

--- helper/link.py
+++ helper/link.py
@@ -1,4 +1,4 @@
-"""
+'''
 Copyright (c) 2015, The Regents of the University of California
 All rights reserved.
 
@@ -12,7 +12,7 @@ modification, are permitted provided that the following conditions are met:
   this list of conditions and the following disclaimer in the documentation
   and/or other materials provided with the distribution.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS'
 AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
 DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
@@ -22,30 +22,30 @@ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
 CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
-"""
+'''
 import os
 
 from helper.base import Component
 
 
 class LLVMLink(Component):
-    """
+    '''
         Component that links the driver bitcode file with all the dependent bitcode files.
-    """
+    '''
 
     def __init__(self, **kwargs):
         self.llvm_bc_out = kwargs.pop('llvm_bc_out', None)
 
     def setup(self):
         if not os.path.exists(self.llvm_bc_out) or not os.path.isdir(self.llvm_bc_out):
-            return "Provided LLVM Bitcode directory:" + str(self.llvm_bc_out) + " does not exist."
+            return 'Provided LLVM Bitcode directory:' + str(self.llvm_bc_out) + ' does not exist.'
         return None
 
     def perform(self):
         return _process_dir(self.llvm_bc_out)
 
     def get_name(self):
-        return "LLVMLink"
+        return 'LLVMLink'
 
     def is_critical(self):
         # Yes, this component is critical
@@ -60,19 +60,26 @@ def _is_bit_code_file(curr_file):
 
 
 def _process_dir(llvm_bc_out):
-    to_test_bc = os.path.join(llvm_bc_out, "vmlinux.llvm.bc")
+    to_test_bc = os.path.join(llvm_bc_out, 'vmlinux.llvm.bc')
+    to_test_ll = os.path.join(llvm_bc_out, 'vmlinux.llvm.ll')
     all_link_files = []
     for root, dirs, files in os.walk(llvm_bc_out):
-        if not len(dirs):
-            link_files = []
-            for file_ in files:
-                link_file = os.path.join(root, file_)
-                if _is_bit_code_file(link_file):
-                    link_files.append(link_file)
-            built_in_bc = os.path.join(root, 'built-in.llvm.bc')
-            os.system('llvm-link-9 ' + " ".join(link_files) + " -o " + built_in_bc)
-            all_link_files.append(built_in_bc)
-    os.system('llvm-link-9 ' + " ".join(all_link_files) + " -o vmlinux.llvm.bc")
-    os.system('llvm-dis-9 vmlinux.llvm.bc -o vmlinux.llvm.ll')
-    print("To check BC file:" + to_test_bc)
+        link_files = []
+        if not len(files):
+            continue
+        built_in_bc = os.path.join(root, 'built-in.llvm.bc')
+        if 'built-in.llvm.bc' in files:
+            files.remove('built-in.llvm.bc')
+        for file_ in files:
+            link_file = os.path.join(root, file_)
+            if _is_bit_code_file(link_file):
+                link_files.append(link_file)
+        if not len(link_files):
+            continue
+        os.system('llvm-link-9 ' + ' '.join(link_files) + ' -o ' + built_in_bc)
+        all_link_files.append(built_in_bc)
+    os.system('llvm-link-9 ' + ' '.join(all_link_files) + ' -o ' + to_test_bc)
+    os.system('llvm-dis-9 ' + to_test_bc + '-o ' + to_test_ll)
+    print('To check BC file:' + to_test_bc)
+    print('To check LL file:' + to_test_ll)
     return True
