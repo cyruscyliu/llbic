314a5beb131a3abb2c624cd4f00b29c3d697649f
cyrus <cyruscyliu@gmail.com>
2019-11-04 11:19:50 +0800

    update license

--- build/base_component.py
+++ build/base_component.py
@@ -1,3 +1,4 @@
+"""
 Copyright (c) 2015, The Regents of the University of California
 All rights reserved.
 
@@ -20,4 +21,47 @@ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
 SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
 CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
+"""
+
+
+class Component(object):
+    """
+        Base component that need to be used by all components to be used.
+    """
+
+    def setup(self):
+        """
+            Perform setup.
+        :return: msg if the Setup Failed else None.
+        """
+        return None
+
+    def perform(self):
+        """
+            Perform the tasks needed for the component.
+        :return:
+        """
+        raise NotImplementedError("Perform needs to be implemented.")
+
+    def cleanup(self):
+        """
+            Perform clean up.
+        :return: msg if the cleanup Failed else None.
+        """
+        return None
+
+    def get_name(self):
+        """
+            Get the name of the component
+        :return: String representing component name.
+        """
+        return "NoName"
+
+    def is_critical(self):
+        """
+            Is the component critical?
+            If true, then failure of this component is treated as fatal.
+        :return: True if the component is critical else false.
+        """
+        return False
