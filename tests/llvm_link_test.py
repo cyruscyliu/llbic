3ceed152d8ec29cc6468aa36ad0d3fb327a1822a
cyrus <cyruscyliu@gmail.com>
2019-11-06 23:41:54 +0800

    fix wrong llvm_bc_out dir

--- tests/llvm_link_test.py
+++ tests/llvm_link_test.py
@@ -18,12 +18,12 @@ def link(**kwargs):
 class TestLLVMLink(TestCase):
     def test_31820(self):
         kwargs = {
-            'llvm_bc_out': os.path.join('linux-3.18.20-llvm-bitcode'),
+            'llvm_bc_out': os.path.join(home, 'linux-3.18.20-llvm-bitcode'),
         }
         link(**kwargs)
 
     def test_2632(self):
         kwargs = {
-            'llvm_bc_out': os.path.join('linux-2.6.32-llvm-bitcode'),
+            'llvm_bc_out': os.path.join(home, 'linux-2.6.32-llvm-bitcode'),
         }
         link(**kwargs)
