72a8d4359c37549294fe833112e71f520cb81da1
Qiang Liu <cyruscyliu@gmail.com>
2019-11-06 16:15:54 +0800

    add llvm link test

--- tests/llvm_link_test.py
+++ tests/llvm_link_test.py
@@ -3,14 +3,18 @@ from unittest import TestCase
 from helper.link import LLVMLink
 
 
+def link(**kwargs):
+    llvm_link = LLVMLink(**kwargs)
+    status = llvm_link.setup()
+    if status is not None:
+        print(status)
+        return
+    llvm_link.perform()
+
+
 class TestLLVMLink(TestCase):
-    def test_mips_31820(self):
+    def test_31820(self):
         kwargs = {
             'llvm_bc_out': '/home/liuqiang/Desktop/linux-3.18.20-llvm-bitcode',
         }
-        llvm_link = LLVMLink(**kwargs)
-        status = llvm_link.setup()
-        if status is not None:
-            print(status)
-            return
-        llvm_link.perform()
+        link(**kwargs)
