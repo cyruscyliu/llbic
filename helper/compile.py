3f6098357582da1705d57ea4e293d83da7d2459f
Cen Zhang <blbllhy@gmail.com>
2019-12-26 14:20:49 +0800

    fix corner case handling unsupported command line compile option

--- helper/compile.py
+++ helper/compile.py
@@ -85,7 +85,7 @@ INVALID_GCC_FLAGS = ['-mno-thumb-interwork', '-fconserve-stack', '-fno-var-track
                      '-fno-delete-null-pointer-checks', '--param=allow-store-data-races=0',
                      '-Wno-unused-but-set-variable', '-Werror=frame-larger-than=1', '-Werror', '-Wall',
                      '-fno-jump-tables', '-nostdinc', '-fno-ipa-sra', '-mno-single-pic-base', 
-                     '-mno-sched-prolog', '-mapcs', '-Wa,-march=all']
+                     '-mno-sched-prolog', '-mapcs', '-Wa,-march=all', '-fno-tree-ch']
 # necessary flags which should be added in the future
 FUTURE_FLAGS = ['-D__linux__']
 # target optimization to be used for llvm
